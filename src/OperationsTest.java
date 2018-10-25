f275ef0581dd3d66b5434e20e6984790465ff37d
karyf325 <43179791+karyf325@users.noreply.github.com>
2018-10-25 16:43:39 -0700

    Add files via upload

--- src/OperationsTest.java
+++ src/OperationsTest.java
@@ -1,9 +1,27 @@
-
+/**
+ * 
+ * @author Karyme Flores
+ *
+ */
 public class OperationsTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int kb; 
+		kb = 6; 
+		System.out.println(kb);
+		kb = kb + 5; 
+		System.out.println(kb + ", Added 5");
+		kb = kb /2; 
+		System.out.println(kb + ", Divided by 2");
+		kb = kb *4; 
+		System.out.println(kb + ", Multiplied by 4");
+		kb = kb -6; 
+		System.out.println(kb + ", Subtracted 6");
+		kb = kb %2; 
+		System.out.println(kb + ", Divided Modularly by 2");
+		
+		
 	}
 
 }
